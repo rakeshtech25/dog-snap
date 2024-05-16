101165684aeb6ee5cac0883da02e0903fce28382
Rakesh <RakeshSag2598@gmail.com>
2024-05-16 23:24:39 +0530

    fix mobile design

--- app/javascript/react/components/DogBreedForm.js
+++ app/javascript/react/components/DogBreedForm.js
@@ -23,37 +23,39 @@ const DogBreedForm = () => {
   };
 
   return (
-    <div className='w-1/2 mx-auto mt-8'>
-      <div className='flex border-2 border-black gap-3 min-h-80'>
-        <div className='w-1/2 p-4 flex items-center'>
-          <form onSubmit={handleSubmit} className="mb-4">
-            <label htmlFor="dog-breed" className="block text-gray-700 font-bold mb-2">Breed</label>
-            <input type="text"
-                   id="dog-breed"
-                   name="dog-breed"
-                   className="border border-gray-300 rounded-md p-2 mr-2 w-80"
-                   value={breed} onChange={(e) => setBreed(e.target.value)
-            }/>
-            <button type="submit"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
-                    disabled={isLoading}>Submit</button>
-          </form>
-        </div>
+    <div id="root">
+      <div className="w-1/2 mx-auto mt-8">
+        <div className="flex flex-col xl:flex-row border-2 border-blue-300 gap-3 min-h-[500px]">
+          <div className="xl:w-1/2 p-4 flex flex-row items-center">
+            <form onSubmit={handleSubmit} className="mb-4 flex flex-col">
+              <label htmlFor="dog-breed" className="text-gray-700 font-bold mb-2">Breed</label>
+              <input type="text"
+                    id="dog-breed"
+                    name="dog-breed"
+                    className="border border-gray-300 rounded-md p-2 mr-2 w-full"
+                    value={breed} onChange={(e) => setBreed(e.target.value)
+              }/>
+              <button type="submit"
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+                      disabled={isLoading}>Submit</button>
+            </form>
+          </div>
 
-        <div className="w-1/2 p-4 flex justify-center items-center">
-          {isLoading ? (
-            <Loader />
-          ) : (
-            <>
-              {image && (
-                <img
-                  src={image}
-                  alt="Dog"
-                  className="rounded-md shadow-md max-w-full object-fit"
-                />
-              )}
-            </>
-          )}
+          <div className="xl:w-1/2 p-4 flex justify-center items-center">
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <>
+                {image && (
+                  <img
+                    src={image}
+                    alt="Dog"
+                    className="rounded-md shadow-md object-fit"
+                  />
+                )}
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
